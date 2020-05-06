e541f1cf30a04f021b3ff424afca6ca2cf5a60fe
GloriaIsaMtz <glo.martinezm21@gmail.com>
2020-05-06 00:45:31 -0500

    Primer commit

--- TDA_ColaCircular_NotPol/ExcepcionColaCirLLena.java
+++
@@ -1,6 +0,0 @@
-public class ExcepcionColaCirLLena extends Exception {
-	public ExcepcionColaCirLLena() {
-		super("La cola ya esta llena");
-	}
-
-}
--- TDA_ColaCircular_NotPol/ExcepcionColaCirVacia.java
+++
@@ -1,6 +0,0 @@
-public class ExcepcionColaCirVacia extends Exception {
-	public ExcepcionColaCirVacia() {
-		super("La cola ya esta Vacia");
-	}
-
-}
